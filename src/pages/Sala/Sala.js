c2b666602ed20c5dd3c46cc98b64943ac8a5b363
Thith-thith <thiththin762@gmail.com>
2021-07-22 00:15:39 +0700

    update second draft

--- src/pages/Sala/Sala.js
+++ src/pages/Sala/Sala.js
@@ -1,9 +1,11 @@
 import { Col, Row, Button } from "antd";
 import React from "react";
 import Contact from "../../components/Contact";
+import WhySALA from "./why";
 
 const student = require("../../lib/img/student.png");
 const sala = require("../../lib/img/Mode.png");
+const why = require("../../lib/img/why2.png");
 
 function Sala() {
   return (
@@ -41,109 +43,54 @@ function Sala() {
         </div>
       </div>
 
-      <div className="container">
-        <div className="sala-purpose-container">
-          <p style={{ fontSize: 26, color: "rgb(95, 99, 104)" }}>
-            Why join Sala?
-          </p>
-          <h2 style={{ fontSize: 56, lineHeight: 1.2 }}>
-            Virtual School for the next generation of learners
-          </h2>
-          <p className="sala-purpose-subtitle">
-            Sala KOOMPI is created to Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat.
-          </p>
-        </div>
-      </div>
-
-      <div className="courses">
+      <div className="sala-white-back">
         <div className="container">
           <Row>
-            <Col span={12}>
-              <img
-                src="https://www.mozilla.org/media/img/firefox/new/desktop/block-mr1-high-res.2dabc380db76.jpg"
-                height="300"
-                width="auto"
-              />
+            <Col span={18}>
+              <div className="sala-purpose-container">
+                <p className="why-join-sala">Why join SALA?</p>
+                <h2>Virtual School for the next generation of learners</h2>
+                <p className="sala-purpose-subtitle">
+                  Sala KOOMPI is created to Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                  ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                  ea commodo consequat.
+                </p>
+              </div>
             </Col>
-            <Col span={12}>
-              <h2>Learn Variety Of Subjects</h2>
-              <p className="intro-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
+            <Col span={6}>
+              <div>
+                <img
+                  src={why.default}
+                  alt=" why join with sala"
+                  className="why-join-salakoompi"
+                />
+              </div>
             </Col>
           </Row>
         </div>
       </div>
 
-      <div className="easy-to-manage">
-        <div className="container">
-          <Row>
-            <Col span={10} offset={2}>
-              <h2>Learn Anywhere Anytime at Your Own Pace </h2>
-              <p className="intro-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </Col>
-            <Col span={10} offset={2}>
-              <img
-                src="https://www.mozilla.org/media/img/firefox/new/desktop/block-mr1-high-res.2dabc380db76.jpg"
-                height="300"
-                width="auto"
-              />
-            </Col>
-          </Row>
-        </div>
+      {/* --------why sala ---------- */}
+      <div>
+        <WhySALA />
       </div>
 
-      <div className="schools">
+      <div className="sala-white-back">
         <div className="container">
           <Row>
-            <Col span={10} offset={2}>
-              <img
-                src="https://www.mozilla.org/media/img/firefox/new/desktop/block-mr1-high-res.2dabc380db76.jpg"
-                height="300"
-                width="auto"
-              />
-            </Col>
-            <Col span={10} offset={2}>
-              <h2>Create Your Own School </h2>
-              <p className="intro-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </Col>
-          </Row>
-        </div>
-      </div>
-
-      <div className="users">
-        <div className="users-container">
-          <Row>
-            <Col span={10} style={{ paddingRight: 60, paddingTop: 50 }}>
-              <h1>
-                Join more than <br />
-                10000 users <br />
-                on Sala
-              </h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-              <Button size="large" type="sala">
-                Explore
-              </Button>
+            <Col span={10}>
+              <div className="sala-koompi-user-details">
+                <h2>Join more than 10000 users on SALA KOOMPI</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <Button size="large" type="sala">
+                  Explore
+                </Button>
+              </div>
             </Col>
             <Col span={14}>
               <img src={student.default} width="auto" height="500" />
