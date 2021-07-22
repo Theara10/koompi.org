30f43ec935006c65422cdd5468bc6d249f35b86e
Theara10 <thearatheng10@gmail.com>
2021-07-22 22:45:29 +0700

    add text content

--- src/pages/Sala/Sala.js
+++ src/pages/Sala/Sala.js
@@ -16,8 +16,8 @@ function Sala() {
                 <div className="sala-koompi-details">
                   <h1>SALA KOOMPI</h1>
                   <p>
-                    A virtual school that allows both students and <br />
-                    teachers to view and provide learning resources.
+                    Virtual platform for students, teachers, and content
+                    creators.
                   </p>
                   <div className="btn-container">
                     <Button size="large" type="primary">
@@ -41,21 +41,28 @@ function Sala() {
         </div>
       </div>
 
-      <div className="container">
-        <div className="sala-purpose-container">
-          <p style={{ fontSize: 26, color: "rgb(95, 99, 104)" }}>
-            Why join Sala?
-          </p>
-          <h2 style={{ fontSize: 56, lineHeight: 1.2 }}>
-            Virtual School for the next generation of learners
-          </h2>
-          <p className="sala-purpose-subtitle">
-            Sala KOOMPI is created to Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat.
-          </p>
+      <div className="elearning-made-easy">
+        <div className="container">
+          <Row>
+            <Col span={10} offset={2}>
+              <h2>E-learning made easy </h2>
+              <p className="intro-description">
+                SALA KOOMPI, formerly called KOOMPI Academy, is an eLearning
+                platform that aims to enable a decentralized and open-ended
+                education for Cambodian students. The website (sala.koompi.com
+                or academy.koompi.com) can be accessed for free by everyone and
+                caters to students as young as ones from the kindergarten level.
+                The default language is in Khmer.
+              </p>
+            </Col>
+            <Col span={10} offset={2}>
+              <img
+                src="https://www.mozilla.org/media/img/firefox/new/desktop/block-mr1-high-res.2dabc380db76.jpg"
+                height="300"
+                width="auto"
+              />
+            </Col>
+          </Row>
         </div>
       </div>
 
@@ -70,12 +77,12 @@ function Sala() {
               />
             </Col>
             <Col span={12}>
-              <h2>Learn Variety Of Subjects</h2>
+              <h2>STEAM subjects in local language</h2>
               <p className="intro-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
+                KOOMPI Sala offers a wide variety of content for users. Our
+                focus and goal are to inspire and encourage Cambodian students
+                to enjoy learning STEAM subjects, inside and outside of their
+                classrooms.
               </p>
             </Col>
           </Row>
@@ -86,12 +93,10 @@ function Sala() {
         <div className="container">
           <Row>
             <Col span={10} offset={2}>
-              <h2>Learn Anywhere Anytime at Your Own Pace </h2>
+              <h2>Take your classroom with you</h2>
               <p className="intro-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
+                Keep track of your study progress and learn anywhere and
+                anytime.
               </p>
             </Col>
             <Col span={10} offset={2}>
